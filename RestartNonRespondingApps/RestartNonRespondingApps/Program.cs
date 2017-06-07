c21813a60841934792e2835e4eb5d3b6f69ee660
David Minker <davidminker@gmail.com>
2017-06-07 22:17:38 +0200

    Initial commit. Created seperate class "DetectTaskStatus" for encapuslating task related methods and field...

---
+++ RestartNonRespondingApps/RestartNonRespondingApps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace RestartNonRespondingApps
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var taskRunnung = new DetectTaskStatus();
+            taskRunnung.Run();
+        }
+    }
+}
+//OneCommanderV2
+//OUTLOOK
